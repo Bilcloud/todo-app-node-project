0b537049db35ffbf1acd1e21ef011b5f0ac2d108
root <root@ip-172-31-18-178.ec2.internal>
2020-11-12 02:39:12 +0000

    kelime

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -53,7 +53,7 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
-                <p className="text-center">No items yet! BULENT</p>
+                <p className="text-center">! BULENT</p>
             )}
             {items.map(item => (
                 <ItemDisplay
